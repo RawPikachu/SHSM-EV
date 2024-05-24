839bb24f990d73a88dcc2e508d6fef92ed67fa68
RawPikachu <102355977z@gmail.com>
2024-05-24 15:23:17 -0400

    Fix async

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from asyncio import sleep, run
 from motor import Motor
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup((17, 27, 22, 23), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-motor = Motor()
-motor.start()
-
-log_count = 0
-
 
 async def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup((17, 27, 22, 23), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    motor = Motor()
+    motor.start()
+
+    log_count = 0
+
     while True:
         if GPIO.input(17):
             motor.stop_running = False
